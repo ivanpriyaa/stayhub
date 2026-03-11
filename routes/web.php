a179661a8d7a0cefbcb54e4c36910da93c94a2f2
Ipong <ivanpriyaa@gmail.com>
2026-03-11 16:18:02 +0700

    update terbaru

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\VillaController;
 use App\Http\Controllers\UserController;
-<<<<<<< HEAD
-=======
->>>>>>> seeder-user
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,7 +28,6 @@ Route::post('/user/update_user/{id}', [UserController::class, 'update_user']);
 Route::get('/user/delete_user/{id}', [UserController::class, 'destroy_user']);
 
 // Customer
-<<<<<<< HEAD
 Route::get('/customer',[CustomerController::class,'customer'])->middleware('auth')->name('customer');
 Route::get('/customer/tambah_customer',[CustomerController::class,'tambah_customer'])->middleware('auth')->name('customer');
 Route::post('/customer/store',[CustomerController::class,'store']);
@@ -46,13 +42,3 @@ Route::get('/villa/edit_villa/{id}', [villaController::class,'edit_villa'])->mid
 Route::post('/villa/update_villa/{id}', [villaController::class,'update_villa']);
 Route::post('/villa/store', [VillaController::class,'store']);
 Route::get('/villa/delete_villa/{id}', [VillaController::class,'destroy_villa']);
-=======
-Route::get('/customer', [CustomerController::class, 'customer'])->middleware('auth')->name('customer');
-Route::get('/customer/tambah_customer', [CustomerController::class, 'tambah_customer'])->middleware('auth')->name('customer');
-Route::post('/customer/store', [CustomerController::class, 'store']);
-
-Route::get('/customer/edit_customer/{id}', [CustomerController::class, 'edit_customer'])->middleware('auth')->name('customer');
-Route::post('/customer/update_customer/{id}', [CustomerController::class, 'update_customer']);
-
-Route::get('/customer/delete_customer/{id}', [CustomerController::class, 'destroy_customer']);
->>>>>>> seeder-user
